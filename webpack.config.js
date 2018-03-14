f45f944a8bbebb5ca39dac3fd0e213cd7484ba02
van hakobyan <van19962013@mail.ru>
2018-03-14 19:41:07 +0400

    init

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
