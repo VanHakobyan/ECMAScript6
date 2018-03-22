481cf66257192e82e9cb08108b30231a9b429227
van hakobyan <van19962013@mail.ru>
2018-03-22 18:14:52 +0400

    add Strings

---
+++ Strings/string.js
@@ -0,0 +1,11 @@
+const name='Van';
+function getAge() {
+    return 22;
+}
+console.log(`Hello my name is ${name}. I am ${getAge()} year old`);
+window.onload=function () {
+
+const html=`<p align="center">MY GITHUB <br>
+    <a href="https//github.com/vanhakobyan">My Github url</a></p>`;
+document.body.innerHTML=html;
+}
